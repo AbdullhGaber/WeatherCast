5f06b2438eaebcf8b0fba139eb8e76be2f798a1c
AbdullhGaber <abdullh760@tahoo.com>
2026-06-22 04:01:18 +0300

    Initial Commit

---
+++ weather_app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather_app
+//
+//  Created by Abdullh Gaber on 22/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
